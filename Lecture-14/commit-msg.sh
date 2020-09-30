1033a87f56a7e79ada27eed700d530e4d3c539c7
Daniel Allen <electricdrop@outlook.com>
2020-09-30 14:32:10 +0300

    Add optional task

---
+++ Lecture-14/commit-msg.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+commit_regex='\[[A-Z]{2,}-[0-9]{1,}\].{3,31}'
+error_msg="Aborting commit. Your commit message doesn't fit the reqired format, e.g. [ABC-12345] Fix some typo"
+
+if ! grep -iqE "$commit_regex" "$1"; then
+    echo "$error_msg" >&2
+    exit 1
+fi
