4849190ff0bd2589e7d238fdff652b5f80350b53
Daniel Allen <electricdrop@outlook.com>
2020-10-05 12:28:01 +0300

    Add path check

--- Lecture-15/git_status_v2.py
+++ Lecture-15/git_status_v2.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
 
 from os import popen, getcwd, path
-from sys import argv
+from sys import argv, exit
 from subprocess import run
 
 
-def is_git_repo(path):
-    return run(f'cd {path} && git rev-parse --is-inside-work-tree >/dev/null 2>&1', shell=True).returncode == 0
+def is_git_repo(repo_path):
+    return run(f'cd {repo_path} && git rev-parse --is-inside-work-tree >/dev/null 2>&1', shell=True).returncode == 0
 
 
-def check_modified(path):
-    output = popen(f'git status {path}').read().split('\n')
+def check_modified(repo_path):
+    output = popen(f'git status {repo_path}').read().split('\n')
     for item in output:
         if item.find('modified') != -1:
             result = item.replace('\tmodified:   ', '')
             print(getcwd() + '/' + result)
 
 
-path = argv[1] if len(argv) >= 2 else getcwd()
+repo_path = argv[1] if len(argv) >= 2 else getcwd()
 
-if path.isdir(path) and is_git_repo(path):
-    check_modified(path)
+if not path.isdir(repo_path) or not is_git_repo(repo_path):
+    print(f"{repo_path} is not a git repo or directory doesn't exist!")
 else:
-    print(f"{path} is not a git repo, or directory doesn't exist!")
+    check_modified(repo_path)
