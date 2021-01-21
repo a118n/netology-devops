675a4d686578650127a8e7b4efa327b4c639da84
Daniel Allen <electricdrop@outlook.com>
2021-01-21 22:18:05 +0300

    Test signed commits

--- Lecture-35/docker_up.sh
+++ Lecture-35/docker_up.sh
@@ -7,3 +7,4 @@ docker run -dit --name centos-es centos:7
 docker exec -it centos-es bash -c 'yum install -y sudo'
 docker run -dit --name centos-ls centos:7
 docker exec -it centos-ls bash -c 'yum install -y sudo'
+echo "Containers started."
