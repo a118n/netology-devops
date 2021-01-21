5fa5aefdf739f5a150d5e41e5a13fb440335627c
Daniel Allen <electricdrop@outlook.com>
2021-01-21 22:15:50 +0300

    Test signed commits

--- Lecture-35/docker_up.sh
+++ Lecture-35/docker_up.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+echo "Starting containers..."
 docker run -dit --name centos-es centos:7
 docker exec -it centos-es bash -c 'yum install -y sudo'
 docker run -dit --name centos-ls centos:7
